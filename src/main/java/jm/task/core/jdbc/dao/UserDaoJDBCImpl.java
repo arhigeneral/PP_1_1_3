6974cca4f88e558e2bf6e66d483dbde498f1b4ba
sad_c <sad_casd@mail.ru>
2024-04-03 15:58:58 +0300

    jobs done

---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,90 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jm.task.core.jdbc.util.Util.getConnection;
+
+public class UserDaoJDBCImpl implements UserDao {
+    public UserDaoJDBCImpl() {
+    }
+    private static Connection connection = getConnection();
+    public void createUsersTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS `mydbtest`.`user` (\n" +
+                "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                "  `name` VARCHAR(45) NOT NULL,\n" +
+                "  `lastName` VARCHAR(45) NOT NULL,\n" +
+                "  `age` INT(3) NOT NULL,\n" +
+                "  PRIMARY KEY (`id`))\n" +
+                "ENGINE = InnoDB\n" +
+                "DEFAULT CHARACTER SET = utf8;\n";
+        try(Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void dropUsersTable() {
+        String sql = "drop table if exists user;";
+        try(Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        String sql = String.format("insert into user (name, lastName, age) values('%s', '%s', %d);",name, lastName, age);
+        try(Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+            System.out.println(String.format("User с именем - %s добавлен в базу данных",name));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void removeUserById(long id) {
+        String sql = String.format("DELETE FROM user WHERE id = %d;",id);
+        try(Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> userList = new ArrayList<>();
+        String sql = "select * from user;";
+        try(Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return userList;
+    }
+
+    public void cleanUsersTable() {
+        String sql = "TRUNCATE TABLE user;";
+        try(Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,24 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "zrhenjq";
+
+    public static Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            return connection;
+        } catch (SQLException e) {
+            System.out.println("there is no connection... Exception!");
+        }
+
+        return null;
+    }
+}
+
