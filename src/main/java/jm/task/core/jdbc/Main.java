948aa67deb77b450289482241f22ddd8cbe6aa59
sad_c <sad_casd@mail.ru>
2024-04-06 21:16:44 +0300

    problem solved

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,26 +1,30 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.util.Util;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        Util.getConnection();
         UserDao userDao = new UserDaoJDBCImpl();
         userDao.createUsersTable();
-
+        Logger LOGGER = Logger.getLogger("USER");
         userDao.saveUser("Name1", "LastName1", (byte) 20);
         userDao.saveUser("Name2", "LastName2", (byte) 25);
         userDao.saveUser("Name3", "LastName3", (byte) 31);
         userDao.saveUser("Name4", "LastName4", (byte) 38);
 
-        System.out.println(userDao.getAllUsers());
+        LOGGER.log(Level.INFO, userDao.getAllUsers().toString());
         userDao.removeUserById(1);
-        System.out.println(userDao.getAllUsers());
+        LOGGER.log(Level.INFO, userDao.getAllUsers().toString());
         userDao.cleanUsersTable();
-        System.out.println(userDao.getAllUsers());
+        LOGGER.log(Level.INFO, userDao.getAllUsers().toString());
         userDao.dropUsersTable();
+
     }
 }
